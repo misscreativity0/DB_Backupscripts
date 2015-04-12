648485332923b837a3c738f3a83fff3b22dbc58e
thesmithgirl <thesmithgirl@users.noreply.github.com>
2015-04-13 00:36:01 +0200

    Update backuppgsql.sh

--- backuppgsql.sh
+++ backuppgsql.sh
@@ -26,7 +26,7 @@ function backup_table()
         echo "Creating Backup $BACKUPDIR/$BACKUPNAME..."
         mkdir -p "$BACKUPDIR"
 
-        pg_dump -U $DB_USER $DB_NAME | xz > "$BACKUPDIR/$BACKUPNAME".xz
+        pg_dump --clean --create -U $DB_USER $DB_NAME | xz > "$BACKUPDIR/$BACKUPNAME".xz
 
         echo "Removing all backups except for the last 7..."
         find "$BACKUPDIR" -type f | sort | head -n -7 | xargs rm
