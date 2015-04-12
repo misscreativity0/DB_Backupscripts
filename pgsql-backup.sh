632e9638bbc57389f306432f02da13d39b949c71
thesmithgirl <thesmithgirl@users.noreply.github.com>
2015-04-12 11:59:55 +0200

    Create pgsql-backup.sh

---
+++ pgsql-backup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# crontab example:
+# 1 4 * * * /usr/local/bin/backuppgsql daily
+# 1 5 * * 0 /usr/local/bin/backuppgsql weekly
+
+if [ "$1" != "daily" ] && [ "$1" != "weekly" ]; then
+        echo "Syntax: $(basename $0) daily|weekly"
+        exit 1
+fi
+
+DATENOW=$(date "+%F_%T")
+DB_USER="root"
+USERNAME="CHANGEME"
+BACKUPROOT="/home/$USERNAME/pgsql_backups/$1/"
+export PGPASSWORD="CHANGEME"
+
+
+function backup_table()
+{
+        DB_NAME="$1"
+        BACKUPDIR="$BACKUPROOT/$DB_NAME"
+        BACKUPNAME="db_backup_"$DB_NAME"_"$DATENOW""
+        LOGFILE="$BACKUPDIR/$BACKUPNAME.log"
+
+        echo "Creating Backup $BACKUPDIR/$BACKUPNAME..."
+        mkdir -p "$BACKUPDIR"
+
+        pg_dump -U $DB_USER $DB_NAME | xz > "$BACKUPDIR/$BACKUPNAME".xz
+
+        echo "Removing all backups except for the last 7..."
+        find "$BACKUPDIR" -type f | sort | head -n -7 | xargs rm
+
+        echo "Contents:"
+        du -h --total "$BACKUPDIR"/*
+}
+
+
+
+# create backups
+backup_table "ttrss"
+backup_table "owncloud"
